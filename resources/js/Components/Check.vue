26c2b15aca1f87ea0c438c82345efc1d3f2b7f4a
Bilal <bhaidar@gmail.com>
2023-01-17 13:31:55 +0200

    show response body in a modal window

--- resources/js/Components/Check.vue
+++ resources/js/Components/Check.vue
@@ -1,6 +1,8 @@
 <script setup>
-
 import {ref} from "vue";
+import { VueFinalModal } from 'vue-final-modal';
+
+import SecondaryButton from "@/Components/SecondaryButton.vue";
 
 const props = defineProps({
   check: Object,
@@ -10,6 +12,8 @@ const createdAt = ref(props?.check?.created_at?.date_time);
 const status = ref(props?.check?.status_text);
 const isSuccessful = ref(props?.check?.is_successful);
 const responseCode = ref(props?.check?.response_code);
+const responseBody = ref(props?.check?.response_body);
+const showBodyModal = ref (false);
 </script>
 
 <template>
@@ -26,7 +30,20 @@ const responseCode = ref(props?.check?.response_code);
         </span>
     </td>
     <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-      View
+      <template v-if="responseBody">
+        <SecondaryButton @click.prevent="showBodyModal = true;">
+          View
+        </SecondaryButton>
+      </template>
+      <template v-else>-</template>
     </td>
   </tr>
+  <teleport to="#modal-area" v-if="showBodyModal">
+    <VueFinalModal v-model="showBodyModal" classes="flex justify-center items-start pt-16 md:pt-24 mx-4"
+                   content-class="relative max-h-full rounded bg-white w-full max-w-2xl p-4 md:p-6"
+                   overlay-class="bg-gradient-to-r from-gray-800 to-gray-500 opacity-50"
+                   :esc-to-close="true">
+      <textarea :value="responseBody" readonly class="border-gray-300 rounded-md w-full bg-gray-50" rows="10" />
+    </VueFinalModal>
+  </teleport>
 </template>
