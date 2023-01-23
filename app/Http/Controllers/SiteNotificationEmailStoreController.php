bd6f87ef6fdb105c0287d6fdffc435b9f9b4c77e
Bilal <bhaidar@gmail.com>
2023-01-23 16:12:29 +0200

    Add email to notification emails on site backend

--- app/Http/Controllers/SiteNotificationEmailStoreController.php
+++ app/Http/Controllers/SiteNotificationEmailStoreController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Site;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class SiteNotificationEmailStoreController extends Controller
 {
-    public function __invoke(Request $request, Site $site)
+    public function __invoke(Request $request, Site $site): RedirectResponse
     {
-        dd($request->email);
+        $site->update([
+           'notification_emails' => Arr::prepend($site->notification_emails ?? [], $request->email),
+        ]);
+        return back();
     }
 }
