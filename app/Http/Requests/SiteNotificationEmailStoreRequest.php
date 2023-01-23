04839444806f58e7fcb6b94cd3f7c3662b88d4b8
Bilal <bhaidar@gmail.com>
2023-01-23 16:27:04 +0200

    Add a policy guard to make sure the user can add a notificaiton channel

--- app/Http/Requests/SiteNotificationEmailStoreRequest.php
+++ app/Http/Requests/SiteNotificationEmailStoreRequest.php
@@ -14,7 +14,7 @@ class SiteNotificationEmailStoreRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return $this->user()->can('storeNotificationChannels', $this->site);
     }
 
     /**
