5de0ba5627a0046252d840957c8e70680282c3bf
Bilal <bhaidar@gmail.com>
2023-01-26 11:37:17 +0200

    Beautify the emails

--- resources/views/emails/endpoint_down.blade.php
+++ resources/views/emails/endpoint_down.blade.php
@@ -1,1 +1,12 @@
-Endpoint {{ $endpoint->id }} went down!
+<x-mail::message>
+    # {{ $endpoint->location }} went down
+
+    It looks like {{ $endpoint->url() }} is down. We'll email and let you know when it's recovered.
+
+    <x-mail::button :url="route('dashboard', $endpoint->site)">
+        Visit dashboard
+    </x-mail::button>
+
+    Thanks,<br>
+    {{ config('app.name') }}
+</x-mail::message>
--- resources/views/emails/endpoint_recovered.blade.php
+++ resources/views/emails/endpoint_recovered.blade.php
@@ -1,1 +1,12 @@
-Endpoint {{ $endpoint->id }} recovered!
+<x-mail::message>
+    # {{ $endpoint->location }} recovered
+
+    Great news! {{ $endpoint->url() }} has recovered.
+
+    <x-mail::button :url="route('dashboard', $endpoint->site)">
+        Visit dashboard
+    </x-mail::button>
+
+    Thanks,<br>
+    {{ config('app.name') }}
+</x-mail::message>
